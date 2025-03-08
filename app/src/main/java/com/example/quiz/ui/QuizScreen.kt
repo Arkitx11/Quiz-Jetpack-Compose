73ebb9209a6204ca051363ad0ee99aa880edb415
Archit Kumar <arkit.x11@gmail.com>
2025-03-08 20:34:03 +0530

    feat: add data layer

---
+++ app/src/main/java/com/example/quiz/ui/QuizScreen.kt
@@ -0,0 +1,24 @@
+package com.example.quiz.ui
+
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun QuizScreen(modifier: Modifier = Modifier) {
+    Scaffold() {
+        
+    }
+}
+
+@Composable
+fun QuizScreenBody(modifier: Modifier = Modifier) {
+    
+}
+
+@Preview
+@Composable
+private fun QuizScreenPreview() {
+    QuizScreen()
+}
