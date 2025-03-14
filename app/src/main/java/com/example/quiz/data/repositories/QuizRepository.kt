c1064ca09b4b4bcc361792ca23e4a8ffd503a46c
Archit Kumar <arkit.x11@gmail.com>
2025-03-14 21:45:11 +0530

    refactor(data): change list name from `questions` to `flutterQuestionsList` for clarity

--- app/src/main/java/com/example/quiz/data/repositories/QuizRepository.kt
+++ app/src/main/java/com/example/quiz/data/repositories/QuizRepository.kt
@@ -2,7 +2,32 @@ package com.example.quiz.data.repositories
 
 import com.example.quiz.data.models.Question
 
-val questions: List<Question> = listOf(
+object QuizRepository {
+    fun getQuestions(index: Int): List<Question> {
+        return when (index) {
+            1 -> flutterQuestionsList
+            2 -> algorithmsQuestionsList
+            3 -> dataStructuresQuestionsList
+            4 -> deepLearningQuestionsList
+            5 -> machineLearningQuestionsList
+            6 -> softComputingQuestionsList
+            else -> videoAnalyticsQuestionsList
+        }
+    }
+
+    fun getTopicsTitle(): List<String> {
+        return listOf(
+            "Flutter",
+            "Algorithms",
+            "Data Structures",
+            "Deep Learning",
+            "Machine Learning",
+            "Soft Computing",
+            "Video Analytics"
+        )
+    }
+}
+
 val flutterQuestionsList: List<Question> = listOf(
     Question(
         id = 1,
